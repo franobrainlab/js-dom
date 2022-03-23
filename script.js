445804c488007ce94764b02b58d7c77a7e093fa3
Frano Rezo <frano.rezo@brainlab.hr>
2022-03-23 19:57:59 +0100

    TODO app

--- script.js
+++ script.js
@@ -1,57 +1,43 @@
-/*  Najbitnije DOM metode:
- *      document.querySelector(); -> Vraća prvi element na kojeg naleti prema CSS selektoru
- *      document.querySelectorAll(); -> Vraća listu svih elemenata prema CSS selektoru
- *      document.getElementById(); -> Vraća element prema 'id' atributu
- *      document.createElement(); -> Kreira i vraća HTML s tagom iz argumenta
- *      element.appendChild(); -> Dodaje element unutar sebe
- *      element.addEventListener(); -> "Slušač" na određeni događaj u DOM-u
- */
+class Todo {
+  text;
+  isDone;
 
-const naslovElement = document.querySelector(".hello");
-
-console.log(naslovElement);
-
-function onClick({ target }) {
-  console.log("text:", target.innerHTML);
+  constructor(text, isDone = false) {
+    this.text = text;
+    this.isDone = isDone;
+  }
 }
 
-const btnElement = document.getElementById("btn");
-// btnElement.addEventListener("click", onClick);
-btnElement.addEventListener("click", ({ target }) => {
-  console.log("ev;", target.innerHTML);
+const todos = [new Todo("Primjer1"), new Todo("Primjer2", true)];
+
+todos.forEach((todo, i) => addNewTodo(todo, i));
+
+// DODAVANJE NOVOG TODO-a
+// const todoForm = document.getElementById("todo-form");
+const todoForm = document.forms.namedItem("todo-form");
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // const newTodoInput = document.getElementById("new-todo");
+  // const formElement = event.target;
+  // const newTodoInput = formElement.querySelector("#new-todo");
+  const inputValue = todoForm.querySelector("input")?.value;
+  if (!inputValue) {
+    return;
+  }
+  const newTodo = new Todo(inputValue);
+  todos.push(newTodo);
+  addNewTodo(newTodo, todos.length - 1);
+  todoForm.reset();
 });
 
-// Kreiranje i append-anje
-const spanElement = document.createElement("span");
-spanElement.innerText = "Alo Marine!";
-console.log(spanElement);
-
-const bodyElement = document.querySelector("body");
-bodyElement.appendChild(spanElement);
-
-// Zadatak: Prikaži user-e na web stranici
-
-const users = [
-  { name: "Marin", age: 27 },
-  { name: "Frano", age: 28 },
-];
-
-const containerElement = document.getElementById("users");
-
-// for (let i = 0; i < users.length; i++) {
-//   const user = users[i];
-//   const liElement = document.createElement("li");
-//   liElement.innerHTML = user.name;
-//   containerElement.appendChild(liElement);
-// }
-
-users.forEach(({ name, age }, i) => {
-  const articleElement = document.createElement("article");
-  articleElement.id = `kartica-${i + 1}`;
-  articleElement.classList.add("card");
-  articleElement.innerHTML = `
-    <strong>${name}</strong>
-    <span>${age}</span>
+function addNewTodo({ text, isDone }, i) {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `
+    <input type="checkbox" name="checkbox-${i}" id="checkbox-${i}"/>
+    <label for="checkbox-${i}">${text}</label>
   `;
-  containerElement.appendChild(articleElement);
-});
+  // <input type="checkbox" name="checkbox-${i}" id="checkbox-${i}" ${isDone ? "checked" : ""}/>
+  const inputElement = divElement.querySelector("input");
+  inputElement.checked = isDone;
+  document.querySelector("main").appendChild(divElement);
+}
